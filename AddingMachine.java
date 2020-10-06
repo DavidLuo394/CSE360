ba66e5728879aa22769e5d9d84a574843b8cefa9
Dawei Luo <71360081+DavidLuo394@users.noreply.github.com>
2020-10-05 22:35:49 -0700

    Update AddingMachine.java
    finished version of AddingMachine.java

--- AddingMachine.java
+++ AddingMachine.java
@@ -2,25 +2,44 @@ package cse360assignment02;
 
 public class AddingMachine {
 	private int total;
+	private String record = "0";
 	  
-	  public AddingMachine () {
-	    total = 0;  // not needed - included for clarity
-	  }
-	  
-	  public int getTotal () {
-	    return 0;
-	  }
-	  
-	  public void add (int value) {
-	  }
-
-	  public void subtract (int value) {
-	  }
-
-	  public String toString () {
-	    return "";
-	  }
-
-	  public void clear() {
-	  }
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return total;
+	}
+	
+	public void add (int value) {
+		total = total + value;
+		record = record + " + " + Integer.toString(value);
+	}
+	
+	public void subtract (int value) {
+		total = total - value;
+		record = record + " - " + Integer.toString(value);
+	}
+	
+	public String toString () {
+		return record;
+	}
+	
+	
+	//in the instruction, "The clear method should clear your "memory"."
+	//if the "memory" means the history of the transactions of this class, then use the 1st ver.
+	//if the "memory" means the memory of this class in JVM, then use the 2nd ver.
+	//1st ver:
+	public void clear() {
+		record = Integer.toString(total);
+	}
+	//2nd ver:
+	/*
+	public void clear() {
+		total = (Integer) null;
+		record = null;
+		System.gc();
+	}
+	*/
 }
